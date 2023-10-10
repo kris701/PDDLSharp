895adeb04d0791993aeef0b067671f5855eb759a
Kristian Skov <kris701kj@gmail.com>
2023-10-10 21:08:01 +0200

    Added a simple mutex detector

--- Simulators.MutexDetector/SimpleMutexDetector.cs
+++ Simulators.MutexDetector/SimpleMutexDetector.cs
@@ -13,16 +13,61 @@ namespace PDDLSharp.Simulators.MutexDetector
         public List<PredicateExp> FindMutexes(PDDLDecl decl)
         {
             List<PredicateExp> mutexCandidates = new List<PredicateExp>();
-            if (decl.Domain.Predicates != null)
-                foreach (var predicate in decl.Domain.Predicates.Predicates)
-                    mutexCandidates.Add(predicate.Copy(null));
 
             foreach(var act in decl.Domain.Actions)
             {
+                Dictionary<PredicateExp, int> balance = new Dictionary<PredicateExp, int>();
 
+                var items = act.Preconditions.FindTypes<PredicateExp>();
+                items.AddRange(act.Effects.FindTypes<PredicateExp>());
+                foreach (var precondition in items)
+                {
+                    var simplified = SimplifyPredicate(precondition);
+                    if (!balance.ContainsKey(simplified))
+                        balance.Add(simplified, 0);
+
+                    if (precondition.Parent is NotExp)
+                        balance[simplified]--;
+                    else
+                        balance[simplified]++;
+                }
+
+                var names = new List<string>();
+                foreach (var key in balance.Keys)
+                    if (!names.Contains(key.Name))
+                        names.Add(key.Name);
+
+                foreach(var name in names)
+                {
+                    bool isGood = true;
+                    foreach (var key in balance.Keys)
+                    {
+                        if (key.Name == name && balance[key] != 0)
+                        {
+                            isGood = false;
+                            break;
+                        }
+                    }
+
+                    if (isGood)
+                    {
+                        if (decl.Domain.Predicates != null)
+                            mutexCandidates.Add(decl.Domain.Predicates.Predicates.First(x => x.Name == name));
+                        else
+                        mutexCandidates.Add(new PredicateExp(name));
+                    }
+                }
             }
 
             return mutexCandidates;
+        }
+
+        private PredicateExp SimplifyPredicate(PredicateExp pred)
+        {
+            var newPred = new PredicateExp(pred.Name);
+            foreach (var arg in pred.Arguments)
+                newPred.Arguments.Add(new NameExp(arg.Name));
+            return newPred;
         }
     }
 }
---
+++ Simulators.MutexDetectors.Tests/SimpleMutexDetectorTests.cs
@@ -0,0 +1,154 @@
+﻿using PDDLSharp.Models.PDDL.Expressions;
+using PDDLSharp.Models.PDDL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PDDLSharp.Models.PDDL.Domain;
+using PDDLSharp.Simulators.MutexDetector;
+using PDDLSharp.Models;
+using PDDLSharp.Models.PDDL.Problem;
+
+namespace PDDLSharp.Simulators.MutexDetectors.Tests
+{
+    [TestClass]
+    public class SimpleMutexDetectorTests
+    {
+        private static NotExp PutInNotNode(IExp node)
+        {
+            var newNode = new NotExp(null);
+            newNode.Child = node;
+            node.Parent = newNode;
+            return newNode;
+        }
+
+        public static IEnumerable<object[]> GetDetectMutexData()
+        {
+            yield return new object[] {
+                new List<ActionDecl>()
+                {
+                    new ActionDecl(
+                        "move",
+                        new ParameterExp(new List<NameExp>(){
+                            new NameExp("?a"),
+                            new NameExp("?b")
+                        }),
+                        new AndExp(new List<IExp>()
+                        {
+                            new PredicateExp("location", new List<NameExp>(){ new NameExp("?a") }),
+                            PutInNotNode(new PredicateExp("location", new List<NameExp>(){ new NameExp("?b") }))
+                        }),
+                        new AndExp(new List<IExp>()
+                        {
+                            PutInNotNode(new PredicateExp("location", new List<NameExp>(){ new NameExp("?a") })),
+                            new PredicateExp("location", new List<NameExp>(){ new NameExp("?b") })
+                        }))
+                },
+                new List<PredicateExp>()
+                {
+                    new PredicateExp("location", new List<NameExp>(){ new NameExp("?pos") })
+                }
+            };
+
+            yield return new object[] {
+                new List<ActionDecl>()
+                {
+                    new ActionDecl(
+                        "move",
+                        new ParameterExp(new List<NameExp>(){
+                            new NameExp("?a"),
+                            new NameExp("?b")
+                        }),
+                        new AndExp(new List<IExp>()
+                        {
+                            new PredicateExp("is-item", new List<NameExp>(){ new NameExp("?a") }),
+                            new PredicateExp("location", new List<NameExp>(){ new NameExp("?a") }),
+                            PutInNotNode(new PredicateExp("location", new List<NameExp>(){ new NameExp("?b") }))
+                        }),
+                        new AndExp(new List<IExp>()
+                        {
+                            PutInNotNode(new PredicateExp("location", new List<NameExp>(){ new NameExp("?a") })),
+                            new PredicateExp("location", new List<NameExp>(){ new NameExp("?b") })
+                        }))
+                },
+                new List<PredicateExp>()
+                {
+                    new PredicateExp("location", new List<NameExp>(){ new NameExp("?pos") })
+                }
+            };
+
+            yield return new object[] {
+                new List<ActionDecl>()
+                {
+                    new ActionDecl(
+                        "move",
+                        new ParameterExp(new List<NameExp>(){
+                            new NameExp("?a"),
+                            new NameExp("?b")
+                        }),
+                        new AndExp(new List<IExp>()
+                        {
+                            new PredicateExp("is-item", new List<NameExp>(){ new NameExp("?a") }),
+                            new PredicateExp("location", new List<NameExp>(){ new NameExp("?a") }),
+                            PutInNotNode(new PredicateExp("location", new List<NameExp>(){ new NameExp("?b") }))
+                        }),
+                        new AndExp(new List<IExp>()
+                        {
+                            PutInNotNode(new PredicateExp("is-item", new List<NameExp>(){ new NameExp("?a") })),
+                            PutInNotNode(new PredicateExp("location", new List<NameExp>(){ new NameExp("?a") })),
+                            new PredicateExp("location", new List<NameExp>(){ new NameExp("?b") })
+                        }))
+                },
+                new List<PredicateExp>()
+                {
+                    new PredicateExp("location", new List<NameExp>(){ new NameExp("?pos") }),
+                    new PredicateExp("is-item", new List<NameExp>(){ new NameExp("?pos") })
+                }
+            };
+
+            yield return new object[] {
+                new List<ActionDecl>()
+                {
+                    new ActionDecl(
+                        "pick",
+                        new ParameterExp(new List<NameExp>(){
+                            new NameExp("?a"),
+                            new NameExp("?b")
+                        }),
+                        new AndExp(new List<IExp>()
+                        {
+                            new PredicateExp("location", new List<NameExp>(){ new NameExp("?a") }),
+                            PutInNotNode(new PredicateExp("location", new List<NameExp>(){ new NameExp("?a") }))
+                        }),
+                        new AndExp(new List<IExp>()
+                        {
+                            PutInNotNode(new PredicateExp("location", new List<NameExp>(){ new NameExp("?a") })),
+                            new PredicateExp("location", new List<NameExp>(){ new NameExp("?b") })
+                        }))
+                },
+                new List<PredicateExp>()
+                {
+                }
+            };
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetDetectMutexData), DynamicDataSourceType.Method)]
+        public void Can_DetectMutex(List<ActionDecl> actions, List<PredicateExp> expectedPredicates)
+        {
+            // ARRANGE
+            IMutexDetectors detector = new SimpleMutexDetector();
+            var decl = new PDDLDecl(new DomainDecl(), new ProblemDecl());
+            decl.Domain.Actions = actions;
+
+            // ACT
+            var mutexes = detector.FindMutexes(decl);
+
+            // ASSERT
+            Assert.AreEqual(expectedPredicates.Count, mutexes.Count);
+            foreach (var expected in expectedPredicates)
+                Assert.IsTrue(mutexes.Any(x => x.Name == expected.Name));
+        }
+    }
+}
