01d25247d3514a864228d83c61a9d426757e29e5
Kristian Skov <kris701kj@gmail.com>
2023-10-15 19:03:24 +0200

    Optimisations

--- PerformanceTests/Program.cs
+++ PerformanceTests/Program.cs
@@ -44,7 +44,7 @@ namespace PerformanceTests
             for (int i = 0; i < number; i++)
             {
                 Console.WriteLine($"Instance {i}");
-                var test = sasParser.Parse(targetSAS);
+                var test = sasParser.Parse(new FileInfo(targetSAS));
 
             }
         }
