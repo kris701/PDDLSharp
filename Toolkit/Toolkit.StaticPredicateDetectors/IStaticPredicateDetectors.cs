2273d2af76240a479519de26d003cecb4f45a19c
Kristian Skov Johansen <kris701kj@gmail.com>
2023-10-18 17:23:21 +0200

    Added a simple static predicate detector and tests

---
+++ Toolkit/Toolkit.StaticPredicateDetectors/IStaticPredicateDetectors.cs
@@ -0,0 +1,15 @@
+﻿using PDDLSharp.Models;
+using PDDLSharp.Models.PDDL.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.StaticPredicateDetectors
+{
+    public interface IStaticPredicateDetectors
+    {
+        public List<PredicateExp> FindStaticPredicates(PDDLDecl decl);
+    }
+}
---
+++ Toolkit/Toolkit.StaticPredicateDetectors/SimpleStaticPredicateDetector.cs
@@ -0,0 +1,32 @@
+﻿using PDDLSharp.Models.PDDL.Expressions;
+using PDDLSharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.StaticPredicateDetectors
+{
+    public class SimpleStaticPredicateDetector : IStaticPredicateDetectors
+    {
+        public List<PredicateExp> FindStaticPredicates(PDDLDecl decl)
+        {
+            List<PredicateExp> statics = new List<PredicateExp>();
+
+            if (decl.Domain != null && decl.Domain.Predicates != null)
+            {
+                var allPredicates = decl.Domain.Predicates.FindTypes<PredicateExp>();
+                foreach(var action in decl.Domain.Actions)
+                {
+                    var effects = action.Effects.FindTypes<PredicateExp>();
+                    allPredicates.RemoveAll(x => effects.Any(y => y.Name == x.Name));
+                }
+                foreach (var pred in allPredicates)
+                    statics.Add(pred.Copy());
+            }
+
+            return statics;
+        }
+    }
+}
