0377ea1779c72938dfa68e74d9d3f8f3bee85f9e
Kristian Skov Johansen <kris701kj@gmail.com>
2023-10-20 21:27:21 +0200

    * Initial action grounder * Fixed a bad default namespace for the static predicate detector

---
+++ Toolkit/Toolkit.Grounders/IGrounder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLSharp.Toolkit.Grounders
+{
+    public interface IGrounder<T, U>
+    {
+        public List<U> Ground(T item);
+    }
+}
