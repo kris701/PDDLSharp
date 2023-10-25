bc7fa066d9e1f6028e026f5006c8b46777e55aa6
Kristian Skov Johansen <kris701kj@gmail.com>
2023-10-25 09:57:11 +0200

    Split code into seperate file

--- Toolkit/Toolkit.Planners/DepthFirstSearch.cs
+++ Toolkit/Toolkit.Planners/DepthFirstSearch.cs
@@ -78,23 +78,5 @@ namespace PDDLSharp.Toolkit.Planners
 
             return new ActionPlan(bestGoal[0].Steps, bestGoal[0].Steps.Count);
         }
-
-        internal class StateMove
-        {
-            public IState State { get; private set; }
-            public List<GroundedAction> Steps { get; private set; }
-
-            public StateMove(IState state, List<GroundedAction> steps)
-            {
-                State = state;
-                Steps = steps;
-            }
-
-            public StateMove(IState state)
-            {
-                State = state;
-                Steps = new List<GroundedAction>();
-            }
-        }
     }
 }
---
+++ Toolkit/Toolkit.Planners/StateMove.cs
@@ -0,0 +1,28 @@
+﻿using PDDLSharp.Models.Plans;
+using PDDLSharp.Toolkit.StateSpace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLSharp.Toolkit.Planners
+{
+    internal class StateMove
+    {
+        public IState State { get; private set; }
+        public List<GroundedAction> Steps { get; private set; }
+
+        public StateMove(IState state, List<GroundedAction> steps)
+        {
+            State = state;
+            Steps = steps;
+        }
+
+        public StateMove(IState state)
+        {
+            State = state;
+            Steps = new List<GroundedAction>();
+        }
+    }
+}
