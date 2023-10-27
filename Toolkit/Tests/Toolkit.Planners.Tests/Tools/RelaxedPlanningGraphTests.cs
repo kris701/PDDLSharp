ccf8b33b4099c7df03540e090beeba814fe9d5c5
Kristian Skov Johansen <kris701kj@gmail.com>
2023-10-27 11:25:31 +0200

    More tests + refactorings

--- Toolkit/Tests/Toolkit.Planners.Tests/Tools/RelaxedPlanningGraphTests.cs
+++ Toolkit/Tests/Toolkit.Planners.Tests/Tools/RelaxedPlanningGraphTests.cs
@@ -19,38 +19,8 @@ using System.Threading.Tasks;
 namespace PDDLSharp.Toolkit.Planners.Tests.Tools
 {
     [TestClass]
-    public class RelaxedPlanningGraphTests
+    public class RelaxedPlanningGraphTests : BasePlannerTests
     {
-        private static Dictionary<string, HashSet<ActionDecl>> _groundedCache = new Dictionary<string, HashSet<ActionDecl>>();
-        private static HashSet<ActionDecl> GetGroundedActions(PDDLDecl decl)
-        {
-            if (_groundedCache.ContainsKey(decl.Domain.Name.Name + decl.Problem.Name.Name))
-                return _groundedCache[decl.Domain.Name.Name + decl.Problem.Name.Name];
-
-            IGrounder<ActionDecl> grounder = new ActionGrounder(decl);
-            var actions = new HashSet<ActionDecl>();
-            foreach (var act in decl.Domain.Actions)
-                actions.AddRange(grounder.Ground(act).ToHashSet());
-            _groundedCache.Add(decl.Domain.Name.Name + decl.Problem.Name.Name, actions);
-            return actions;
-        }
-
-        private static Dictionary<string, PDDLDecl> _declCache = new Dictionary<string, PDDLDecl>();
-        private static PDDLDecl GetPDDLDecl(string domain, string problem)
-        {
-            if (_declCache.ContainsKey(domain + problem))
-                return _declCache[domain + problem];
-
-            IErrorListener listener = new ErrorListener();
-            IParser<INode> parser = new PDDLParser(listener);
-            var decl = new PDDLDecl(
-                parser.ParseAs<DomainDecl>(new FileInfo(domain)),
-                parser.ParseAs<ProblemDecl>(new FileInfo(problem))
-                );
-            _declCache.Add(domain + problem, decl);
-            return decl;
-        }
-
         [TestMethod]
         [DataRow("TestData/gripper/domain.pddl", "TestData/gripper/prob01.pddl", 3)]
         [DataRow("TestData/gripper/domain.pddl", "TestData/gripper/prob06.pddl", 3)]
