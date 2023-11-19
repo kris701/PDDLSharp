7e4a4e56c2c20837b42ab296250a01f6a1914cfc
Kristian Skov Johansen <kris701kj@gmail.com>
2023-11-19 11:01:42 +0100

    Added a deconstructor for imply nodes

---
+++ Tests/Translators.Tests/Tools/ImplyDeconstructorTests.cs
@@ -0,0 +1,87 @@
+﻿using PDDLSharp.Models.PDDL;
+using PDDLSharp.Models.PDDL.Domain;
+using PDDLSharp.Models.PDDL.Expressions;
+using PDDLSharp.Models.PDDL.Problem;
+using PDDLSharp.Translators.Grounders;
+using PDDLSharp.Translators.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLSharp.Translators.Tests.Tools
+{
+    [TestClass]
+    public class ImplyDeconstructorTests
+    {
+        public static IEnumerable<object[]> DeconstrucImplyData_Valid()
+        {
+            yield return new object[] {
+                new AndExp(new List<IExp>(){
+                    new ImplyExp(
+                        new PredicateExp("ant", new List<NameExp>(){ new NameExp("?a") }),
+                        new PredicateExp("con", new List<NameExp>(){ new NameExp("?b") })
+                    )
+                }),
+                new AndExp(new List<IExp>(){
+                    new OrExp(new List<IExp>()
+                    {
+                        new NotExp(new PredicateExp("ant", new List<NameExp>(){ new NameExp("?a") })),
+                        new AndExp(new List<IExp>(){
+                            new PredicateExp("ant", new List<NameExp>(){ new NameExp("?a") }),
+                            new PredicateExp("con", new List<NameExp>(){ new NameExp("?b") })
+                        })
+                    })
+                })
+            };
+
+            yield return new object[] {
+                new AndExp(new List<IExp>(){
+                    new ImplyExp(
+                        new PredicateExp("ant", new List<NameExp>(){ new NameExp("?a") }),
+                        new PredicateExp("con", new List<NameExp>(){ new NameExp("?b") })
+                    ),
+                    new ImplyExp(
+                        new PredicateExp("ant2", new List<NameExp>(){ new NameExp("?a") }),
+                        new PredicateExp("con2", new List<NameExp>(){ new NameExp("?b") })
+                    )
+                }),
+                new AndExp(new List<IExp>(){
+                    new OrExp(new List<IExp>()
+                    {
+                        new NotExp(new PredicateExp("ant", new List<NameExp>(){ new NameExp("?a") })),
+                        new AndExp(new List<IExp>(){
+                            new PredicateExp("ant", new List<NameExp>(){ new NameExp("?a") }),
+                            new PredicateExp("con", new List<NameExp>(){ new NameExp("?b") })
+                        })
+                    }),
+                    new OrExp(new List<IExp>()
+                    {
+                        new NotExp(new PredicateExp("ant2", new List<NameExp>(){ new NameExp("?a") })),
+                        new AndExp(new List<IExp>(){
+                            new PredicateExp("ant2", new List<NameExp>(){ new NameExp("?a") }),
+                            new PredicateExp("con2", new List<NameExp>(){ new NameExp("?b") })
+                        })
+                    })
+                })
+            };
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(DeconstrucImplyData_Valid), DynamicDataSourceType.Method)]
+        public void Can_DeconstructImply(INode input, INode expected)
+        {
+            // ARRANGE
+            (input as AndExp).Children[0].Parent = input;
+            (expected as AndExp).Children[0].Parent = expected;
+            var deconstructor = new ImplyDeconstructor();
+
+            // ACT
+            var result = deconstructor.DeconstructImplies(input);
+
+            // ASSERT
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Translators/Tools/ImplyDeconstructor.cs
@@ -0,0 +1,40 @@
+﻿using PDDLSharp.Models.PDDL;
+using PDDLSharp.Models.PDDL.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLSharp.Translators.Tools
+{
+    public class ImplyDeconstructor
+    {
+        public bool Aborted { get; set; } = false;
+
+        public T DeconstructImplies<T>(T node) where T : INode
+        {
+            var copy = node.Copy();
+            var implies = copy.FindTypes<ImplyExp>();
+            foreach (var imply in implies)
+            {
+                if (Aborted) break;
+                if (imply.Parent is IWalkable walk)
+                {
+                    var newNode = new OrExp(imply.Parent);
+                    newNode.Options.Add(new NotExp(newNode, imply.Antecedent));
+                    newNode.Options.Add(new AndExp(newNode, new List<IExp>() {
+                        imply.Antecedent,
+                        imply.Consequent
+                    }));
+
+                    walk.Replace(imply, newNode);
+                }
+                else
+                    throw new Exception("Parent for imply deconstruction must be a IWalkable!");
+            }
+
+            return (T)copy;
+        }
+    }
+}
