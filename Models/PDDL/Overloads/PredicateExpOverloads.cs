a15991e4d24d646a5e5d3106c040948604a2299e
Kristian Skov Johansen <kris701kj@gmail.com>
2024-03-21 10:38:25 +0100

    Predicate exp overloads

---
+++ Models/PDDL/Overloads/PredicateExpOverloads.cs
@@ -0,0 +1,41 @@
+﻿using PDDLSharp.Models.PDDL.Domain;
+using PDDLSharp.Models.PDDL.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDLSharp.Models.PDDL.Overloads
+{
+    public static class PredicateExpOverloads
+    {
+        public static bool CanOnlyBeSetToTrue(this PredicateExp self, DomainDecl domain)
+        {
+            var result = true;
+
+            foreach (var action in domain.Actions)
+            {
+                var effects = action.Effects.FindNames(self.Name);
+                if (effects.Any(x => x.Parent is NotExp))
+                    return false;
+            }
+
+            return result;
+        }
+
+        public static bool CanOnlyBeSetToFalse(this PredicateExp self, DomainDecl domain)
+        {
+            var result = true;
+
+            foreach (var action in domain.Actions)
+            {
+                var effects = action.Effects.FindNames(self.Name);
+                if (effects.Any(x => x.Parent is not NotExp))
+                    return false;
+            }
+
+            return result;
+        }
+    }
+}
